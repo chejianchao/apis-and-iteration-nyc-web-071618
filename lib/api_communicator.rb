6aae320d6a18d671f07515f54da6e71494a2822d
chejianchao <820992376@qq.com>
2018-07-17 20:13:29 +0000

    automatically backed up by learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -15,7 +15,7 @@ def get_character_movies_from_api(character)
     character_hash["results"].each {|result|
       if result["name"].downcase.include?( character )
         result["films"].each {|film_url|
-          res[result["name"] = helper(film_url)
+          res[result["name"]] = helper(film_url)
         }
       end
     }
